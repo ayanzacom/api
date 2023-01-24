d95cbd942f3148ba751637b2dfbd5895ac5e8129
JSimko4 <jakuboss1@gmail.com>
2023-01-24 09:51:22 +0100

    update discord example

--- examples/DiscordCustomersExample/index.ts
+++ examples/DiscordCustomersExample/index.ts
@@ -9,14 +9,14 @@ app.use(cors({ origin: true }));
 
 app.get('/', async (req, res) => {
     const api = new AyanzaClient({
-        token: '5f69357a5432ad5500703ffeb6f4551d67c1d635e33aa56253dcc5a6f361df07',
+        token: '78594c508b28ac08cda118e5fb80574fac6d3c33f5d21447f5e55244bd15fcb5',
         apiTarget: 'http://127.0.0.1:5005/knoweveryone-4e500/europe-west3/api'
     })
 
-    const space = await api.space.create({title: 'Customers database'});
-    const widget1 = await api.widget.create({parentId: space.id, title: 'Customer 1'});
-    const widget2 = await api.widget.create({parentId: space.id, title: 'Customer 2'});
-    const widget3 = await api.widget.create({parentId: space.id, title: 'Customer 3'});
+    const space = await api.space.create('Customers database');
+    const widget1 = await api.widget.create('Customer 1', space.id);
+    const widget2 = await api.widget.create('Customer 2',space.id);
+    const widget3 = await api.widget.create('Customer 3',space.id);
 
     // try delete
     // const deleted = await api.widget.delete(widget2.id); // how to get response code ?
