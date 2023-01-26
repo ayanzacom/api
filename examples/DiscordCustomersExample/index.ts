56cfbb2337ca5bfc4946df5665c2a50528f215db
JSimko4 <jakuboss1@gmail.com>
2023-01-26 13:14:13 +0100

    .env

--- examples/DiscordCustomersExample/index.ts
+++ examples/DiscordCustomersExample/index.ts
@@ -1,18 +1,20 @@
 import {AyanzaClient} from "../../src/lib/AyanzaClient";
 import {WidgetProperties} from "../../src/lib/Widget/WidgetApi";
+import * as dotenv from 'dotenv'
 
 const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = 3098
-
 app.use(cors({ origin: true }));
 
+const dotEnvPath = '../../.env'
+dotenv.config({ path: dotEnvPath})
 
 app.get('/', async (req, res) => {
     const api = new AyanzaClient({
-        token: 'cd52df6e1f06186d78480de0279bc3ef5fd344fa5d3528f7f56349036784b97f',
-        apiTarget: 'http://127.0.0.1:5005/knoweveryone-4e500/europe-west3/api'
+        token: process.env.API_TOKEN as string,
+        apiTarget: 'http://127.0.0.1:5005/knoweveryone-4e500/europe-west4/api'
     })
 
     const team = await api.space.create('Customers', null, true);
