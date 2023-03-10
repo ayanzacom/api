135f4ceac7941e323ab3da507e9c5b9228dee18c
JSimko4 <xsimkoj2@stuba.sk>
2023-03-10 11:51:53 +0100

    Add property can contain value (needed for relation property type)

--- src/lib/Schema/SchemaApi.ts
+++ src/lib/Schema/SchemaApi.ts
@@ -4,6 +4,7 @@ export type SchemaAddPropertyBodyRequest = {
     id?: string,
     type: string,
     name: string,
+    value?: string | null
     options?: any,
 };
 
@@ -27,10 +28,11 @@ export class SchemaApi {
     get(id: string): Promise<SchemaGetResponse> {
         return this.transport(`schema/${id}`);
     }
-    addProperty(schemaId: string, type: string, name: string, propertyId?: string, options?: any): Promise<SchemaAddPropertyResponse> {
+    addProperty(schemaId: string, type: string, name: string, propertyId?: string, options?: any, value?: string | null,): Promise<SchemaAddPropertyResponse> {
         const reqBody: SchemaAddPropertyBodyRequest = {name, type}
         if(propertyId) reqBody.id = propertyId
         if(options) reqBody.options = options
+        if(value) reqBody.value = value
         return this.transport(`schema/${schemaId}/property`, {method: 'POST', body: reqBody});
     }
     deleteProperty(schemaId: string, propertyId: string): Promise<unknown> {
